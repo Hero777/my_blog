0622848a1bf3c838b6c92de04d1c319ef038474a
Hero777 <clickker@mail.ru>
2023-10-27 22:54:54 +0200

    - adding /posts/new GET in PostController

--- src/main/java/com/example/springbootblogadd/controllers/PostController.java
+++ src/main/java/com/example/springbootblogadd/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.example.springbootblogadd.controllers;
 
+import com.example.springbootblogadd.models.Account;
 import com.example.springbootblogadd.models.Post;
+import com.example.springbootblogadd.services.AccountService;
 import com.example.springbootblogadd.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +18,9 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
 
@@ -27,6 +32,19 @@ public class PostController {
             Post post = optionalPost.get();
             model.addAttribute("post", post);
             return "post";
+        } else {
+            return "404";
+        }
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model) {
+        Optional<Account> optionalAccount = accountService.findByEmail("user.user@domain.com");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
         } else {
             return "404";
         }
